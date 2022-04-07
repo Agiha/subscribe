586e87948b757557ffe3d972dfd00f4fe82548a6
agiha <agiha1028@gamil.com>
2022-04-07 10:23:26 +0800

    bottom

--- src/page/AboutUs/index.tsx
+++ src/page/AboutUs/index.tsx
@@ -2,8 +2,36 @@ import React from "react";
 import "./index.css";
 import man from "../../assets/header/man.png";
 import HeaderBox from "../../compoment/Header";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import SourceIcon from "@mui/icons-material/Source";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 export default function Chat() {
+	const FAQsData = [
+		{
+		  title:'What is Substance?',
+		  content:'Here for the first time? See how Substance can help from sales and marketing, to customer engagement and support.'
+		},
+		{
+		  title:'How does Substance work?',
+		  content:'Here for the first time? See how Substance can help from sales and marketing, to customer engagement and support.'
+		} ,{
+		  title:'Can I talk to real person to get my questions answered?',
+		  content:'Here for the first time? See how Substance can help from sales and marketing, to customer engagement and support.'
+		},
+		{
+		  title:'In which country Substance available?',
+		  content:'Here for the first time? See how Substance can help from sales and marketing, to customer engagement and support.'
+		},
+	
+	  ];
+	const [expanded, setExpanded] = React.useState<string | false>(false);
 	const NotificationsData =[
 		{
 			head:man,
@@ -21,66 +49,58 @@ export default function Chat() {
 			noticeContent:'Joined the Substance group.',
 
 
-		},
-		{
-			head:man,
-			Source:false,
-			name:'Hi, Max',
-			time:"a minute ago",
-			noticeContent:'Here is some tips to “Getting started with Substance”',
-
-		},
-		{
-			head:man,
-			Source:false,
-			name:'Brian Tylor',
-			time:"a minute ago",
-			noticeContent:'Change an issues from “In progress” to “Review”',
-
-		},
-		{
-			head:man,
-			Source:false,
-			name:'Brian Tylor',
-			time:"a minute ago",
-			noticeContent:'Change an issues from “In progress” to “Review”',
-
-		},
-		{
-			head:man,
-			Source:false,
-			name:'Brian Tylor',
-			time:"a minute ago",
-			noticeContent:'Change an issues from “In progress” to “Review”',
-
-		},
-		{
-			head:man,
-			Source:true,
-			name:'Brian Tylor',
-			time:"a minute ago",
-			noticeContent:'Change an issues from “In progress” to “Review”',
-
-		},
-		{
-			head:man,
-			Source:true,
-			name:'Brian Tylor',
-			time:"a minute ago",
-			noticeContent:'Change an issues from “In progress” to “Review”',
-
-		},
+		}
 	]
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
   return (
     <div>
       <HeaderBox></HeaderBox>
       <div className="NotificationsBigBox">
         <div className="Title">
-          <div className="Notifications">Notifications</div>
-          <div className="Mark">Mark all read</div>
+          <div className="Notifications">Subscription</div>
         </div>
-        <div className="ListBox">
-          {NotificationsData.map((item) => {
+		<div className="TextFieldBigBox">
+			<div className="TextFieldBox">
+			<div className="TextField">
+				<Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '550px' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" fullWidth label="subscribe"  />
+    </Box>	
+			</div>
+			<div className="btnBox">
+			<Stack spacing={2} direction="row">
+      <Button variant="outlined" className="btn">subscribe</Button>
+    </Stack>
+			</div>
+	
+		</div>
+		</div>
+		<div className="problemBox">
+         {FAQsData.map((item,index) =>{
+           return(
+             <div>
+      <Accordion expanded={expanded ===`panel${index}`} onChange={handleChange(`panel${index}`)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: '80%', flexShrink: 0 }}>{item.title}<Button variant="outlined" className="btn">subscribe</Button></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+           
+<div className="ListBox">
+          {NotificationsData.map((item) => { 
             return (
               <div className="noticeBox">
                 <div className="Img">
@@ -108,7 +128,49 @@ export default function Chat() {
             );
           })}
         </div>
+		  
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+             </div>
+           )
+         })}
+       </div>
+		
+        
       </div>
+	  <div className="bottomBigBox">
+		  <div className="add">
+			  <div className="selectionTitle">
+			  <div className="Title">
+			  Make a selection
+			  </div>
+			  <div className="titleConcent">
+			  Enter the email address where you'd like to receive these newsletters.
+			  </div>
+		  </div>
+		  <div className="EmailBigBox">
+			  <div className="EmailBox">
+				  <div className="Email">
+					  <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '300px' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Email address" variant="outlined" />
+    </Box>	  
+				  </div>
+		<div className="btn">
+		<Button variant="outlined" >Subscribe</Button>
+		</div>
+			  </div>
+		  </div>
+		  </div>
+		  
+	  </div>
     </div>
   );
 }
